88d5f471081b1162efe69750fb480119ea24ab35
Penn Oser <pennoser@gmail.com>
2019-09-14 08:36:41 -0500

    Cleaned up Code

--- alwaysonbottom.ps1
+++ alwaysonbottom.ps1
@@ -1,28 +1,25 @@
 param([string]$process)
 
-Function MoveWindowtoBottom() {
-
 $WindowAPIDef = @"
-	[DllImport("user32.dll", CharSet = CharSet.Unicode)]
-	public static extern IntPtr FindWindow(IntPtr sClassName, String sAppName);
+		[DllImport("user32.dll", CharSet = CharSet.Unicode)]
+		public static extern IntPtr FindWindow(IntPtr sClassName, String sAppName);
 	
-	[DllImport("user32.dll", CharSet = CharSet.Unicode)]
-	public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
+		[DllImport("user32.dll", CharSet = CharSet.Unicode)]
+		public static extern IntPtr SetWindowPos(IntPtr hWnd, int hWndInsertAfter, int x, int Y, int cx, int cy, int wFlags);
 	
-	public static void MovetoBottom(IntPtr hWnd) {
-		SetWindowPos(hWnd, 1, 0, 0, 0, 0, 0x0010 | 0x0002 | 0x0001);
-	}
-	
-	
+		public static void MovetoBottom(IntPtr hWnd) {
+			SetWindowPos(hWnd, 1, 0, 0, 0, 0, 0x0010 | 0x0002 | 0x0001);
+		}
 "@
 
-$WindowAPI = Add-Type -Namespace Win32 -Name Funcs -MemberDefinition $WindowAPIDef -PassThru
+Function MoveWindowtoBottom() {
+	$WindowAPI = Add-Type -Namespace Win32 -Name Funcs -MemberDefinition $WindowAPIDef -PassThru
 	
-$WindowName = Get-Process $Process |where {$_.mainWindowTItle}
+	$WindowName = Get-Process $Process |where {$_.mainWindowTItle}
 
-$GetWindow = $WindowAPI::FindWindow([IntPtr]::Zero, $WindowName.MainWindowTitle)
+	$GetWindow = $WindowAPI::FindWindow([IntPtr]::Zero, $WindowName.MainWindowTitle)
 
-$WindowAPI::MovetoBottom($GetWindow)
+	$WindowAPI::MovetoBottom($GetWindow)
 }
 
 
